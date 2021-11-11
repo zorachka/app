133831a396cfb9c9bd2275815d6f394e87e5dfef
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2021-11-11 15:16:29 +0400

    feat: add relay, diactoros and basic middlewares

--- public/index.php
+++ public/index.php
@@ -2,6 +2,13 @@
 
 declare(strict_types=1);
 
+use FastRoute\RouteCollector;
+use Laminas\Diactoros\ServerRequestFactory;
+use Middlewares\FastRoute;
+use Middlewares\RequestHandler;
+use Relay\Relay;
+use function FastRoute\simpleDispatcher;
+
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 $containerBuilder = new \DI\ContainerBuilder();
@@ -12,4 +19,14 @@ $containerBuilder->addDefinitions([
 ]);
 $container = $containerBuilder->build();
 
-echo 'Hello, world!';
+$routes = simpleDispatcher(function (RouteCollector $r) {
+    $r->get('/', function () {
+        echo 'Hello, world!';
+        exit;
+    });
+});
+$middlewareQueue[] = new FastRoute($routes);
+$middlewareQueue[] = new RequestHandler();
+
+$requestHandler = new Relay($middlewareQueue);
+$requestHandler->handle(ServerRequestFactory::fromGlobals());
