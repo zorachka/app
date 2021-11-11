d118524415d2f95b666e9d7cdff2fe03aa1402e0
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2021-11-11 15:11:58 +0400

    feat: add php di

--- public/index.php
+++ public/index.php
@@ -4,4 +4,12 @@ declare(strict_types=1);
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
+$containerBuilder = new \DI\ContainerBuilder();
+$containerBuilder->useAutowiring(false);
+$containerBuilder->useAnnotations(false);
+$containerBuilder->addDefinitions([
+
+]);
+$container = $containerBuilder->build();
+
 echo 'Hello, world!';
